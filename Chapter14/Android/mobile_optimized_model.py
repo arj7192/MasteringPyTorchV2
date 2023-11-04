bc5bbbfd3f4e2e8dd9d5193b61c0ed312b628a5f
Ashish Jha <arj7192@gmail.com>
2023-11-04 14:59:10 +0000

    update mobile optimize code for chapter 14

--- Chapter14/Android/mobile_optimized_model.py
+++ Chapter14/Android/mobile_optimized_model.py
@@ -1,4 +1,6 @@
+import torch
 from torch.utils.mobile_optimizer import optimize_for_mobile
 
+traced_model = torch.jit.load('../../Chapter13/traced_convnet.pt')
 optimized_traced_model = optimize_for_mobile(traced_model)
-optimized_traced_model._save_for_lite_interpreter("./optimized_for_mobile_traced_model.pt")
+optimized_traced_model._save_for_lite_interpreter("./app/src/main/assets/optimized_for_mobile_traced_model.pt")
